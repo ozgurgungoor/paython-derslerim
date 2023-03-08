7b4b3c63b0584ff3ec583a649a2635a20bb86b5b
ozgurgunngor <127289182+ozgurgunngor@users.noreply.github.com>
2023-03-08 12:50:09 +0300

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,45 @@
+print("""Merhaba
+Dünya
+
+
+!!!""")
+
+
+print("Merhaba \nDünya")
+
+print("Merhaba \t\t\tDünya")
+
+mesaj = "Merhaba dünyaa 1"
+
+mesaj2 = "Merhaba dünyaa"
+
+print(mesaj +" "+ mesaj2)
+
+print(mesaj2[0:7])
+
+
+print("merhaba " * 10)
+
+
+
+
+isim = "Özgür"
+
+yas = "19"
+
+print("{} , {} yaşındadır".format(isim,yas))
+
+
+
+
+
+mesaj3 = "Üniversiteliyim"
+
+print("{} , {} dedi...".format(isim,mesaj3)) 
+
+
+
+
+
+
+print(f"{isim} {mesaj} dedi")
